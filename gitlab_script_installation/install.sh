1cd8f53d5147c71f72110072e603af9a01d9a75a
peznuss <peznuss@Mac-mini-de-peznuss.local>
2025-03-04 18:12:09 -0600

    add arch diagrams and gitlab_script

--- gitlab_script_installation/install.sh
+++ gitlab_script_installation/install.sh
@@ -2,3 +2,16 @@
 
 #config the Swap on file /etc/dphys-swapfile
 
+# define swap size of 4GB
+SWAP_SIZE_MB=4096
+
+echo "🚀 Change swap to ${SWAP_SIZE_MB}MB ..."
+
+sudo dphys-swapfile swapoff
+
+sudo sed -i "s/^CONF_SWAPSIZE=.*/CONF_SWAPSIZE=$SWAP_SIZE_MB/" /etc/dphys-swapfile
+
+sudo dphys-swapfile setup
+sudo dphys-swapfile swapon
+
+free -h
