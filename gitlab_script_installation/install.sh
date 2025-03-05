fb3f7ffa6ec2fd4a7dd0b26d671588a158f7f58b
peznuss <peznuss@Mac-mini-de-peznuss.local>
2025-03-04 18:30:59 -0600

    add arch diagrams and gitlab_script

--- gitlab_script_installation/install.sh
+++ gitlab_script_installation/install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#config the Swap on file /etc/dphys-swapfile
+# Config the Swap on file /etc/dphys-swapfile
 
-# define swap size of 4GB
+# Define swap size of 4GB
 SWAP_SIZE_MB=4096
-CONF_MAXSWAP=4096
 
 echo "🚀 Change swap and max swap to ${SWAP_SIZE_MB}MB ..."
 
@@ -16,3 +15,15 @@ sudo dphys-swapfile setup
 sudo dphys-swapfile swapon
 
 free -h
+
+# Install GitLab
+
+sudo apt-get install curl openssh-server ca-certificates apt-transport-https perl
+
+curl https://packages.gitlab.com/gpg.key | sudo tee /etc/apt/trusted.gpg.d/gitlab.asc
+
+sudo apt-get install -y postfix
+
+sudo curl -sS https://packages.gitlab.com/install/repositories/gitlab/raspberry-pi2/script.deb.sh | sudo bash
+
+sudo EXTERNAL_URL="https://gitlab.peznuss.com" apt-get install gitlab-ce
