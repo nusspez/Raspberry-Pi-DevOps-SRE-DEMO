12a96e84bdc6665806d606edb713d5c7f81f674b
peznuss <peznuss@Mac-mini-de-peznuss.local>
2025-03-05 00:54:47 -0600

    add install.sh to 32 bit and 64 bit

--- gitlab_script_installation/install_64_bit.sh
+++ gitlab_script_installation/install_64_bit.sh
@@ -49,6 +49,6 @@ sudo gitlab-ctl reconfigure
 
 echo "✅ Instalación completa."
 
-cat /etc/gitlab/initial_root_password
+sudo cat /etc/gitlab/initial_root_password
 
 echo # WARNING: This file will be automatically deleted in 24 hours.
