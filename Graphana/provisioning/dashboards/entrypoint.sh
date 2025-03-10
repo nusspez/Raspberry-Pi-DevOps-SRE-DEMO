3ebd39e24a28d4602fcfd8d8b534c665bd636c94
peznuss <peznuss@Mac-mini-de-peznuss.local>
2025-03-10 13:15:14 -0600

    add docker compose to install node exporter

--- Graphana/provisioning/dashboards/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Reemplazando variables en el JSON del dashboard..."
-envsubst < /etc/grafana/provisioning/dashboards/RaspberryPiDashboards.json > /etc/grafana/provisioning/dashboards/dashboards_fixed.json
-
-# Ejecutar Grafana
-exec /run.sh
