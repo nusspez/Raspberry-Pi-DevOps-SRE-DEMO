9f173f5a931166fad0e584de22dac93c6b68e603
peznuss <peznuss@Mac-mini-de-peznuss.local>
2025-03-10 19:16:32 -0600

    add docker compose to install node exporter

--- gitlab_script_installation/install_64_bit.sh
+++ gitlab_script_installation/install_64_bit.sh
@@ -92,7 +92,7 @@ echo "🔑 Contraseña: $SECURE_PASSWORD"
 
 #crear un proyecto de Gitlab 
 
-curl --request POST "https://$IP/api/v4/projects" \
+curl --request POST "http://$IP/api/v4/projects" \
      --header "PRIVATE-TOKEN: MiSuperToken123" \
      --form "name=Ansible" \
      --form "visibility=private"
