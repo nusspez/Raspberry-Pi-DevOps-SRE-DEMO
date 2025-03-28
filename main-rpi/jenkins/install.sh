9d5cd0478b4ff146d3a79756768be0e3c20edd7a
peznuss <peznuss@Mac-mini-de-peznuss.local>
2025-03-27 18:16:28 -0600

    add jenkins

--- main-rpi/jenkins/install.sh
+++ main-rpi/jenkins/install.sh
@@ -3,7 +3,7 @@ set -e  # Salir si algún comando falla
 export DEBIAN_FRONTEND=noninteractive  # Evita confirmaciones interactivas
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y openjdk-11-jdk
+sudo apt install default-jdk
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
